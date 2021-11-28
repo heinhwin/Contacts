47217449b74af95a7e5c817f4e6193d8af7c59ca
Hein <heinhwin23@gmail.com>
2021-11-27 21:47:13 -0800

    added loading spinner screen

--- app/src/main/java/com/hein/contacts/MainActivity.java
+++ app/src/main/java/com/hein/contacts/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.lusfold.spinnerloading.SpinnerLoading;
+
 import java.util.ArrayList;
 import java.util.List;
 
